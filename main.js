96d05d86e6a6b1156d00c81554a898c84e84232c
lali <gulisano.lara@gmail.com>
2022-08-15 12:27:20 -0300

    create grid and paint multicolor

--- main.js
+++ main.js
@@ -5,26 +5,33 @@ button.addEventListener('click', ()=> {
     let userChoice = prompt("How many boxes do you want? (up to 100)");
     container.innerText = "";
     if (userChoice <= 100) {
-    for (let i=1; i<=userChoice; i++) {
+        createCells(userChoice);
+        div.innerText = `${userChoice} x ${userChoice}`
+
+   
+   /*     for (let i=1; i<=userChoice; i++) {
         let cell = document.createElement('div');
         cell.textContent = `${i}`;
         container.appendChild(cell);
-    }} else {
-        alert("Up to 100 only!")
+    }*/} else {
+        alert("Enter a number up to 100 only!")
     }
 })
 
-function createCells() {
-for (let i=1; i <257; i++) {
-    let div = document.createElement('div');
-    div.setAttribute('class', 'cell')
-    container.appendChild(div);
-    div.textContent = `${i}`;
-    div.addEventListener ('mouseenter', () => {
+function createCells(a) {
+for (let i=1; i <a*a; i++) {
+    const div = document.createElement('div');          // 1) Create cells in DOM
+    div.setAttribute('class', 'cell')                 // 2) Assign a class to it ('cell')
+    container.appendChild(div);                       // 3) Append it to the container
+    container.style.gridTemplateColumns = `repeat(${a}, ${980/a}px)`; /*16*/ 
+    container.style.gridTemplateRows = `repeat(${a}, ${980/a}px)`;      /*64 */
+    //div.textContent = `${i}`;
+    div.addEventListener ('mouseenter', () => {           // 4) Create event when mouse passes over cells
         let rgbOne = Math.floor((Math.random() *255)+1);
         let rgbTwo = Math.floor((Math.random() *255)+1);
         let rgbThree = Math.floor((Math.random() *255)+1);
         div.style.background = `rgb(${rgbOne}, ${rgbTwo}, ${rgbThree})`;
     })
-}}
-createCells();
+}
+}
+createCells(16);
